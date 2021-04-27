80a5b70e0e58832540e335c1eb609fb22792758b
Lucas Ribas <ribasprojectt@gmail.com>
2021-04-26 22:16:35 -0300

    nova versao - trafic shaping LMH

--- r2a/r2a_averagethroughput.py
+++ r2a/r2a_averagethroughput.py
@@ -10,10 +10,9 @@ class R2A_AverageThroughput(IR2A):
 
     def __init__(self, id):
         IR2A.__init__(self, id)
-        #self.throughputs = []
+        self.throughputs = []
         self.request_time = 0
         self.qi = []
-        self.vazao = 0
         self.buffer = []
         self.flag = False
         self.selected_qi = 46980
@@ -22,13 +21,15 @@ class R2A_AverageThroughput(IR2A):
         self.media = 0
         self.soma_qi = 0
 
-        self.inicio = 0
-
+        self.buff_flag = 0
         self.estabilidade = 0
-
         self.pior_caso = False
-
         self.libera_aumento = True
+        self.percent = 40 # %
+        self.lista_qi_selects = []
+        self.limite_bff_baixo = 0
+        self.banda_alta = 0
+        self.banda_baixa = 0
 
     def handle_xml_request(self, msg):
         self.request_time = time.perf_counter()
@@ -39,83 +40,102 @@ class R2A_AverageThroughput(IR2A):
         parsed_mpd = parse_mpd(msg.get_payload())
         self.qi = parsed_mpd.get_qi()
 
-
         t = time.perf_counter() - self.request_time
 
-        #self.throughputs.append(msg.get_bit_length() / t)
-
-        self.vazao = msg.get_bit_length() / t
-
-        
+        self.throughputs.append(msg.get_bit_length() / t)
 
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
-        self.buffer.append(self.whiteboard.get_amount_video_to_play())
 
+        self.buffer.append(self.whiteboard.get_amount_video_to_play())
         print(f"BUFFER {self.buffer}\n")
-        print(f"VAZAO {self.vazao} ===================================================\n")
+        
+        vazao = "{:.2f}".format(self.throughputs[-1])
+
+        print(f"VAZAO {vazao} 100% ===================================================\n")
 
         self.request_time = time.perf_counter()
 
         if not self.flag:
-            self.inicio += 1
             for i in self.qi:
-                if (self.vazao * 0.4) > i:
+                if (self.throughputs[-1] * 0.8) > i:
                     self.selected_qi = i
-            if self.inicio == 20:
-                self.flag = True
+            self.flag = True
 
         if self.flag:
-            tam = len(self.buffer)
-            self.estabilidade += 1
-            if self.vazao >= self.selected_qi and (self.estabilidade >= 5) and self.pior_caso == False:
+            tam_buff = len(self.buffer)
+            tam_through = len(self.throughputs)
+
+            if tam_through > 1:
+                if self.throughputs[-1] < self.throughputs[-2]:
+                    self.percent = (100 * self.throughputs[-1]) / self.throughputs[-2]
+                    # Porcentagem da banda, em relação a anterior. Se ela for menor
+            
+            if self.percent >= 70: # %
+                self.banda_alta += 1
+            
+            if tam_buff > 1:
+                if self.buffer[-1] > self.buffer[-2] and self.banda_alta >= 6:
+                    self.banda_alta = 0
+                    pos = self.qi.index(self.selected_qi)
+                    self.selected_qi = self.qi[pos+1]
+                    self.estabilidade = 1
+
+            
+            if (self.throughputs[-1] * 0.6) >= self.selected_qi and (self.estabilidade == 1) and self.percent >= 40 and not self.pior_caso:
                 self.estabilidade = 0
-                if tam > 1:
-                    if self.buffer[-1] > self.buffer[-2] or self.buffer[-1] >= 10:
+                if tam_buff > 1:
+                    if self.buffer[-1] > self.buffer[-2] or self.buffer[-1] >= 12:
                         self.libera_aumento = True
 
                     if self.libera_aumento:
-                        if self.buffer[-1] > self.buffer[-2] or self.vazao > self.selected_qi * 2:
-                            pos = self.qi.index(self.selected_qi)
-                            self.selected_qi = self.qi[pos+4]
+                        if self.buffer[-1] > self.buffer[-2]:
+                            self.buff_flag += 1
+                            if self.buff_flag == 3:
+                                self.buff_flag = 0
+                                pos = self.qi.index(self.selected_qi)
+                                self.selected_qi = self.qi[pos+1]
+                        else:
+                            if self.buff_flag > 0:
+                                self.buff_flag -= 1
                         
-                        if self.buffer[-1] >= 15 or self.vazao > self.selected_qi * 1.5:
+                        if self.buffer[-1] > self.buffer[-2] and self.buffer[-1] > 30:
                             pos = self.qi.index(self.selected_qi)
-                            self.selected_qi = self.qi[pos+2]
-
-                    if self.buffer[-1] <= 5:
-                        self.libera_aumento = False
-                        self.selected_qi = self.qi[2]
+                            self.selected_qi = self.qi[pos+1]
                 
             else:
-                if self.buffer[-1] <= 2:
-                    self.libera = False
-                    self.selected_qi = self.qi[1]
-                    #self.pior_caso = True
-                else:
-                    if self.buffer[-1] <= 4:
+                if tam_buff > 1:
+                    if self.buffer[-1] <= 4 and self.buffer[-1] < self.buffer[-2]:
                         self.libera = False
-                        self.selected_qi = self.qi[2]
-                        #self.pior_caso = True
-                    else:
-                        if self.buffer[-1] <= 6:
-                            self.libera = False
-                            self.selected_qi = self.qi[4]
-                        else:
-                            if self.buffer[-1] <= 7:
-                                self.libera = False
-                                self.selected_qi = self.qi[5]
-                # if self.buffer[-1] >= 12:
-                #     self.pior_caso = False
+                        self.selected_qi = self.qi[0]
+                        self.pior_caso = True
+                        self.limite_bff_baixo += 1
+                    
+                    if self.buffer[-1] >= 6:
+                        self.pior_caso = False
                 
+                if self.percent <= 25: # %
+                    self.banda_baixa += 1
+
+                if self.banda_baixa >= 3:
+                    self.banda_baixa = 0
+                    pos = self.qi.index(self.selected_qi)
+                    if pos > 0:
+                        self.selected_qi = self.qi[pos-1]
+                    else:
+                        self.selected_qi = self.qi[0]
+        
         self.soma_qi = self.soma_qi + self.qi.index(self.selected_qi)
         self.media = (self.soma_qi) / self.cont
         self.cont += 1
 
         self.media = "{:.2f}".format(self.media)
 
-        print(f"MEDIA QI: {self.media} ===================================================\n")
+        self.lista_qi_selects.append(self.qi.index(self.selected_qi))
+
+        print(f"MEDIA QI: {self.media} ||||||| BUFFER PERTO DE ZERO: {self.limite_bff_baixo} vzs ============\n")
+        print(f"QUALIDADES SELECIONADAS: {self.lista_qi_selects}\n")
 
         msg.add_quality_id(self.selected_qi)
         self.send_down(msg)
@@ -123,9 +143,9 @@ class R2A_AverageThroughput(IR2A):
     def handle_segment_size_response(self, msg):
         t = time.perf_counter() - self.request_time
 
-        #self.throughputs.append(msg.get_bit_length() / t)
+        self.throughputs.append(msg.get_bit_length() / t)
 
-        self.vazao = msg.get_bit_length() / t
+        self.throughputs[-1] = msg.get_bit_length() / t
 
         self.send_up(msg)
 
