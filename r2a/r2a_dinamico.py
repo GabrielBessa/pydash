2c0e9aed9317e53b9ecaac4f96ed52a820ff8f93
Lucas Ribas <ribasprojectt@gmail.com>
2021-04-30 23:54:50 -0300

    branch testes_abr_d pra master

--- r2a/r2a_dinamico.py
+++ r2a/r2a_dinamico.py
@@ -1,3 +1,8 @@
+""" 
+    Gabriel Cunha Bessa Vieira 16/0120811
+    Lucas Junior Ribas - 16/0052289
+"""
+
 from r2a.ir2a import IR2A
 from player.parser import *
 import time
@@ -22,6 +27,16 @@ class R2A_Dinamico(IR2A):
         self.limite_bff_baixo = 0
         self.inicio = 0
 
+    def can_upgrade(self, current_throughput, previous_throughputs, current_qi, qi_list):
+        # Compara se a vazao atual eh maior que a media das 5 ultimas
+        if current_throughput > mean(previous_throughputs[(len(previous_throughputs)-6):-2]):
+            # Compara o valor da qualidade atual com as 3 ultimas para ver se pode mandar subir
+            if current_qi == mean(qi_list[(len(qi_list)-4):-1]):
+                return True
+            else:
+                return False
+        
+
     def handle_xml_request(self, msg):
 
         self.request_time = time.perf_counter()
@@ -45,57 +60,61 @@ class R2A_Dinamico(IR2A):
 
         self.request_time = time.perf_counter()
 
+        # Flag para controlar qualidade na primeira iteracao
         if not self.flag:
-            self.inicio += 1
-            for i in self.qi:
-                if self.throughputs[-1] * 0.4 > i:
-                    self.selected_qi = i
-            if self.inicio == 2:
-                self.flag = True
-
+            self.selected_qi = self.qi[6]            
+            self.flag = True
+        # Operacao depois que a primeira qualidade foi setada
         if self.flag:
             pos = self.qi.index(self.selected_qi)
             if pos < 19:
                 tempo_down_prox = self.qi[pos+1] / self.throughputs[-1]
 
-            min_buffer = 30
+            min_buffer = 40
             if pos < 5:
-                min_buffer = 15
+                min_buffer = 20
 
-            if tempo_down_prox < 0.60 and self.buffer[-1] >= min_buffer:
+            if tempo_down_prox < 0.50 and self.buffer[-1] >= min_buffer:
                 if pos < 19:
-                    self.selected_qi = self.qi[pos+1]
+                    result = self.can_upgrade(self.throughputs[-1],
+                                                     self.throughputs,
+                                                      self.qi.index(self.selected_qi),
+                                                        self.lista_qi_selects)
+                    print(result)
+                    if result == True:
+                        self.selected_qi = self.qi[pos+1]
 
             self.temp_dow = self.selected_qi / self.throughputs[-1]
 
-            tam_QIs = len(self.lista_qi_selects)
-            if tam_QIs > 1:
-                pos = self.qi.index(self.selected_qi)
-                tam = len(self.buffer)
-                if tam > 1:
-                    if self.lista_qi_selects[-1] == pos:
-                        if pos > 12:
-                            if self.temp_dow > 0.70 and self.buffer[-1] < 20 and (self.buffer[-1] < self.buffer[-2] or self.buffer[-1] <= 5):
-                                pos = self.qi.index(self.selected_qi)
-                                if pos > 0:
-                                    self.selected_qi = self.qi[pos-1]
-                                else:
-                                    self.selected_qi = self.qi[0]
+            
+            pos = self.qi.index(self.selected_qi)
+            tam = len(self.buffer)
+            if tam > 1:
+                # Comparacao do tempo de download com o tamanho do buffer(19) e os dois ultimos valores do buffer
+                if pos > 12:
+                    if self.temp_dow > 0.50 and self.buffer[-1] < 20 and (self.buffer[-1] < self.buffer[-2] or self.buffer[-1] <= 5):
+                        pos = self.qi.index(self.selected_qi)
+                        if pos > 0:
+                            self.selected_qi = self.qi[pos-1]
                         else:
-                            if pos > 8:
-                                if self.temp_dow > 0.70 and self.buffer[-1] < 15 and (self.buffer[-1] < self.buffer[-2] or self.buffer[-1] <= 5):
-                                    pos = self.qi.index(self.selected_qi)
-                                    if pos > 0:
-                                        self.selected_qi = self.qi[pos-1]
-                                    else:
-                                        self.selected_qi = self.qi[0]
+                            self.selected_qi = self.qi[0]
+                else:
+                    # Comparacao do tempo de download com o tamanho do buffer(14) e os dois ultimos valores do buffer
+                    if pos > 8:
+                        if self.temp_dow > 0.50 and self.buffer[-1] < 15 and (self.buffer[-1] < self.buffer[-2] or self.buffer[-1] <= 5):
+                            pos = self.qi.index(self.selected_qi)
+                            if pos > 0:
+                                self.selected_qi = self.qi[pos-1]
                             else:
-                                if self.temp_dow > 0.60 and self.buffer[-1] < 11 and (self.buffer[-1] < self.buffer[-2] or self.buffer[-1] <= 5):
-                                    pos = self.qi.index(self.selected_qi)
-                                    if pos > 0:
-                                        self.selected_qi = self.qi[pos-1]
-                                    else:
-                                        self.selected_qi = self.qi[0]
+                                self.selected_qi = self.qi[0]
+                    else:
+                        # Comparacao do tempo de download com o tamanho do buffer(10) e os dois ultimos valores do buffer
+                        if self.temp_dow > 0.50 and self.buffer[-1] < 11 and (self.buffer[-1] < self.buffer[-2] or self.buffer[-1] <= 5):
+                            pos = self.qi.index(self.selected_qi)
+                            if pos > 0:
+                                self.selected_qi = self.qi[pos-1]
+                            else:
+                                self.selected_qi = self.qi[0]
 
             print(f"Temp Down: {self.temp_dow} ||| Temp Down Prox: {tempo_down_prox} ================================\n")
 
